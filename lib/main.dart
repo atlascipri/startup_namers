18d55a6a510b46dd61251fb4f17193a0766d5984
atlascipri <atlasciprianoo@gmail.com>
2023-04-13 18:43:01 -0300

    'Adição da visualização em grade'

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
   final Set<WordPair> _saved = new Set<WordPair>();
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+  bool _isListMode = true;
 
   @override
   Widget build(BuildContext context) {
@@ -33,6 +34,18 @@ class RandomWordsState extends State<RandomWords> {
         title: const Text('Startup Name Generator'),
         actions: <Widget>[
           new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved),
+          ToggleButtons(
+            children: [
+              Icon(Icons.list),
+              Icon(Icons.grid_on),
+            ],
+            isSelected: [_isListMode, !_isListMode],
+            onPressed: (index) {
+              setState(() {
+                _isListMode = index == 0;
+              });
+            },
+          ),
         ],
       ),
       body: _buildSuggestions(),
@@ -40,18 +53,22 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildSuggestions() {
-    return new ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (BuildContext _context, int i) {
-          if (i.isOdd) {
-            return const Divider();
-          }
-          final int index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return _buildRow(_suggestions[index]);
-        });
+    if (_isListMode) {
+      return new ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemBuilder: (BuildContext _context, int i) {
+            if (i.isOdd) {
+              return const Divider();
+            }
+            final int index = i ~/ 2;
+            if (index >= _suggestions.length) {
+              _suggestions.addAll(generateWordPairs().take(10));
+            }
+            return _buildRow(_suggestions[index]);
+          });
+    } else {
+      return _buildGrid();
+    }
   }
 
   Widget _buildRow(WordPair pair) {
@@ -74,6 +91,69 @@ class RandomWordsState extends State<RandomWords> {
             _saved.add(pair);
           }
         });
+      },
+    );
+  }
+
+  Widget _buildGrid() {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
+      itemCount: _suggestions.length,
+      itemBuilder: (BuildContext context, int index) {
+        final pair = _suggestions[index];
+        final alreadySaved = _saved.contains(pair);
+
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Card(
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  if (alreadySaved) {
+                    _saved.remove(pair);
+                  } else {
+                    _saved.add(pair);
+                  }
+                });
+              },
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      pair.asPascalCase,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: InkWell(
+                        onTap: () {
+                          setState(() {
+                            if (alreadySaved) {
+                              _saved.remove(pair);
+                            } else {
+                              _saved.add(pair);
+                            }
+                          });
+                        },
+                        child: Icon(
+                          alreadySaved ? Icons.favorite : Icons.favorite_border,
+                          color: alreadySaved ? Colors.red : Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
       },
     );
   }
